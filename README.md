aa2ed84c0fbb8b49b5bc4786751130d714ceb6cc
hqzing <hqzing@outlook.com>
2025-11-21 23:41:01 +0800

    第一个版本

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
 # ohos-busybox
+
+本项目为 OpenHarmony 平台编译了 busybox，并发布预构建包。
+
+## 获取预构建包
+
+前往 [release 页面](https://github.com/Harmonybrew/ohos-busybox/releases) 获取。
+
+## 用法
+**1\. 在鸿蒙 PC 中使用**
+
+因系统安全规格限制等原因，暂不支持通过“解压 + 配 PATH” 的方式使用这个软件包。
+
+你可以尝试将 tar 包打成 hnp 包再使用，详情请参考 [DevBox](https://gitcode.com/OpenHarmonyPCDeveloper/devbox) 的方案。
+
+**2\. 在鸿蒙开发板中使用**
+
+用 hdc 把它推到设备上，然后以“解压 + 配 PATH” 的方式使用。
+
+示例：
+```sh
+hdc file send busybox-1.37.0-ohos-arm64.tar.gz /data
+hdc shell
+
+cd /data
+tar -zxf busybox-1.37.0-ohos-arm64.tar.gz
+export PATH=$PATH:/data/busybox-1.37.0-ohos-arm64/bin
+
+# 现在可以使用 busybox 命令了
+```
+
+**3\. 在 [鸿蒙容器](https://github.com/hqzing/docker-mini-openharmony) 中使用**
+
+在容器中用 curl 下载这个软件包，然后以“解压 + 配 PATH” 的方式使用。
+
+示例：
+```sh
+docker run -itd --name=ohos ghcr.io/hqzing/docker-mini-openharmony:latest
+docker exec -it ohos sh
+
+cd /root
+curl -L -O https://github.com/Harmonybrew/ohos-busybox/releases/download/1.37.0/busybox-1.37.0-ohos-arm64.tar.gz
+tar -zxf busybox-1.37.0-ohos-arm64.tar.gz -C /opt
+export PATH=$PATH:/opt/busybox-1.37.0-ohos-arm64/bin
+
+# 现在可以使用 busybox 命令了
+```
+
+## 从源码构建
+
+**1\. 手动构建**
+
+需要用一台 Linux x64 服务器来运行项目里的 build.sh，以实现 busybox 的交叉编译。
+
+这里以 Ubuntu 24.04 x64 作为示例：
+```sh
+sudo apt update && sudo apt install -y build-essential unzip
+./build.sh
+```
+
+**2\. 使用流水线构建**
+
+如果你熟悉 GitHub Actions，你可以直接复用项目内的工作流配置，使用 GitHub 的流水线来完成构建。
+
+这种情况下，你使用的是 GitHub 提供的构建机，不需要自己准备构建环境。
+
+只需要这么做，你就可以进行你的个人构建：
+1. Fork 本项目，生成个人仓
+2. 在个人仓的“Actions”菜单里面启用工作流
+3. 在个人仓提交代码或发版本，触发流水线运行
---
+++ build.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+set -e
+
+# 当前工作目录。拼接绝对路径的时候需要用到这个值。
+WORKDIR=$(pwd)
+
+# 如果存在旧的目录和文件，就清理掉
+rm -rf *.tar.gz \
+    busybox-1.37.0.tar.bz2 \
+    busybox-1.37.0 \
+    ohos-sdk \
+    busybox-1.37.0-ohos-arm64
+
+# 准备 ohos-sdk
+mkdir ohos-sdk
+curl -L -O https://repo.huaweicloud.com/openharmony/os/6.0-Release/ohos-sdk-windows_linux-public.tar.gz
+tar -zxf ohos-sdk-windows_linux-public.tar.gz -C ohos-sdk
+cd ohos-sdk/linux
+unzip -q native-*.zip
+cd ../..
+
+# 准备源码
+curl -L -O https://busybox.net/downloads/busybox-1.37.0.tar.bz2
+tar -jxf busybox-1.37.0.tar.bz2
+cd busybox-1.37.0
+
+# 打一个鸿蒙适配的小补丁
+patch -p1 < ../0001-adapt-to-ohos.patch
+
+# 生成默认配置
+make defconfig
+
+# 一些难以适配的功能直接禁用掉
+sed -i 's/CONFIG_SHA1_HWACCEL=y/# CONFIG_SHA1_HWACCEL is not set/' .config
+sed -i 's/CONFIG_FEATURE_UTMP=y/# CONFIG_FEATURE_UTMP is not set/' .config
+sed -i 's/CONFIG_FEATURE_SU_CHECKS_SHELLS=y/# CONFIG_FEATURE_SU_CHECKS_SHELLS is not set/' .config
+sed -i 's/CONFIG_HOSTID=y/# CONFIG_HOSTID is not set/' .config
+sed -i 's/CONFIG_HUSH=y/# CONFIG_HUSH is not set/' .config
+
+# 编译 busybox
+make -j$(nproc) \
+    CONFIG_PREFIX=${WORKDIR}/busybox-1.37.0-ohos-arm64 \
+    CC=${WORKDIR}/ohos-sdk/linux/native/llvm/bin/aarch64-unknown-linux-ohos-clang \
+    LD=${WORKDIR}/ohos-sdk/linux/native/llvm/bin/ld.lld \
+    AR=${WORKDIR}/ohos-sdk/linux/native/llvm/bin/llvm-ar \
+    STRIP=${WORKDIR}/ohos-sdk/linux/native/llvm/bin/llvm-strip \
+    HOSTCC=gcc \
+    HOSTLD=ld
+cd ..
+
+# 手动进行“安装”
+mkdir -p busybox-1.37.0-ohos-arm64/bin
+cp busybox-1.37.0/busybox busybox-1.37.0-ohos-arm64/bin/
+
+# 履行开源义务，将 license 随制品一起发布
+cp busybox-1.37.0/LICENSE busybox-1.37.0-ohos-arm64/
+cp busybox-1.37.0/AUTHORS busybox-1.37.0-ohos-arm64/
+
+# 打包最终产物
+tar -zcf busybox-1.37.0-ohos-arm64.tar.gz busybox-1.37.0-ohos-arm64
